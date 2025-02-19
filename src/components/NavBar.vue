587e41a124b64cdcfb2ec3f7559adb72d1262eb2
DavidOlmos03 <juanda_201001@hotmail.com>
2025-02-18 22:46:00 -0500

    toggleMenu bug fixed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,14 @@
 <script setup>
+    import {ref} from 'vue';
     import LiNavbar from '@/components/LiNavbar.vue';
 
     const list1 = ['library','mathematics', 'physics'];
     const list2 = ['engineering', 'others'];
+
+    const isMenuOpen = ref(false);
+    const toggleMenu = () => {
+        isMenuOpen.value = !isMenuOpen.value;
+    }
 </script>
 
 <template>
@@ -24,7 +30,11 @@
                         <router-link to="/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</router-link>
                     </li>
                     <li>
-                        <button id="mega-menu-full-dropdown-button" data-collapse-toggle="mega-menu-full-dropdown" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded md:w-auto hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Library <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <button 
+                        id="mega-menu-full-dropdown-button" 
+                        @click="toggleMenu()"
+                        type="button" 
+                        class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded md:w-auto hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Library <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
 
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                         </svg></button>
@@ -38,7 +48,7 @@
                 </ul>
             </div>
         </div>
-        <div id="mega-menu-full-dropdown" class="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
+        <div v-show="isMenuOpen" class="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
             <div class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
                 <ul>
                     <LiNavbar v-for="item in list1" :key="item" :area="item" />
